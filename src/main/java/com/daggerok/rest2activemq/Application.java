22aac8896c305306d69bee45e629c0ea5638a273
Maksim Kostromin <daggerok@gmail.com>
2015-11-27 04:54:52 +0200

    replace with lambda^ code review

--- src/main/java/com/daggerok/rest2activemq/Application.java
+++ src/main/java/com/daggerok/rest2activemq/Application.java
@@ -1,8 +1,5 @@
 package com.daggerok.rest2activemq;
 
-import java.io.File;
-import java.util.logging.Logger;
-import javax.jms.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,47 +11,54 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.jms.*;
+import java.io.File;
+import java.util.logging.Logger;
+
 @EnableJms
 @SpringBootApplication
 public class Application {
-	static Logger logger = Logger.getLogger(Application.class.getName());
-	
-	static final String DESTINATION = "from-rest-to-jms-mailbox";
-	
-	@Description("this is where we start")
+    static final String DESTINATION = "from-rest-to-jms-mailbox";
+
+    static Logger logger = Logger.getLogger(Application.class.getName());
+
+    @Description("this is where we start")
+
     public static void main(String[] args) {
         // Clean out any ActiveMQ data from a previous run
         FileSystemUtils.deleteRecursively(new File("activemq-data"));
         SpringApplication.run(Application.class, args);
     }
-    
+
+    @Description("required bean named 'myJmsContainerFactory' :)")
+
     @Bean
-    @Description("where are should be defined bean named 'myJmsContainerFactory' :)")
     JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
         SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         return factory;
     }
-    
+
+    @Description("producing message. for testing run: curl localhost:8080/send/Hello,%20World!")
+
     @RestController
     @RequestMapping(produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
-    @Description("producing message. for testing run: curl localhost:8080/send/Hello,%20World!")
     static class JmsMessageService {
-    	@Autowired
-    	JmsTemplate jmsTemplate;
-    	
-    	@RequestMapping("/send/{message}")
-    	String send(@PathVariable("message") String message) {
-            jmsTemplate.send(DESTINATION, new MessageCreator() {
-            	@Override public Message createMessage(Session session) throws JMSException {
-            		return session.createTextMessage(message); }});
-            
-    		return String.format("message '%s' sent.\n", message);
-    	}
+        @Autowired
+        JmsTemplate jmsTemplate;
+
+        @RequestMapping("/send/{message}")
+        String send(@PathVariable("message") String message) {
+            jmsTemplate.send(DESTINATION, session ->
+                session.createTextMessage(message));
+
+            return String.format("message '%s' sent.\n", message);
+        }
     }
-    
+
+    @Description("message receiver listener, it consumes any messages from ${DESTINATION}")
+
     @Component
-    @Description("simple receiver-listener, it consums any messages from ${DESTINATION}")
     static class Receiver {
         @JmsListener(destination = DESTINATION, containerFactory = "myJmsContainerFactory")
         void receive(String message) {
